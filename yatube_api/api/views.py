0d54b2f9b05fb387e58356ef1ba86c8a8ce46f17
Орлов Сергей <jokeblablo@gmail.com>
2023-04-06 13:58:55 +0300

    check

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -24,11 +24,15 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated,
                           IsAuthorOrReadOnly]
+    
+    def get_post(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post
 
     def get_queryset(self):
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        post = self.get_post()
         return post.comments
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        post = self.get_post()
         serializer.save(author=self.request.user, post=post)
